3222786551d00e9198f0ed6319513d05e23a1b71
Dmitriy <murinetsdy@gmail.com>
2018-11-08 16:56:11 +0300

    правки

--- dialog.cpp
+++ dialog.cpp
@@ -15,7 +15,12 @@
 #include <QUrlQuery>
 #include <QPair>
 
-#include <foldercompressor.h>
+//#include <foldercompressor.h>
+//#define QUAZIP_STATIC
+
+//#include "quazip/quazip.h"
+//#include "quazip/quazipfile.h"
+
 
 Dialog::Dialog(QWidget *parent) :
     QDialog(parent),
@@ -1169,11 +1174,9 @@ void Dialog::on_pushButtonFindWnd_clicked()
     }
 }
 
+
 void Dialog::handleNamReplyFinished(QNetworkReply* repl)
 {
-
-
-
         QList<QNetworkReply::RawHeaderPair> headPairs = repl->rawHeaderPairs();
 
 //        foreach(QNetworkReply::RawHeaderPair  p,  headPairs){
@@ -1284,11 +1287,24 @@ void Dialog::handleNamReplyFinished(QNetworkReply* repl)
 
                         QString rootPath = ui->lineEditRootPath->text();
                         QString filePath = rootPath + "/download/" + filename;
-                        QString fileOutPath = rootPath + "/download/";
 
-                        FolderCompressor fc;
-                        fc.decompressFolder(filePath, fileOutPath);
-                        //unZip(filePath, fileOutPath);
+                        QFileInfo  fi(filename);
+                        QString fileOutPath = rootPath + "/" + fi.baseName();
+                        QDir().mkdir(fileOutPath);
+
+
+
+                        QProcess *unZipProc = new QProcess(this);
+                        unZipProc->setWorkingDirectory(fileOutPath);
+                        unZipProc->start("unzip", QStringList(filePath));
+                        unZipProc->waitForStarted(-1);
+                        //unZipProc->waitForFinished(-1);
+                        connect(unZipProc,  QOverload<int, QProcess::ExitStatus>::of(&QProcess::finished),
+                            [=](int exitCode, QProcess::ExitStatus exitStatus){
+                                qDebug("lala");
+                            });
+
+
                     }
                 }
             }
@@ -1472,7 +1488,13 @@ void Dialog::installProgram(QString path)
     appendLogString("download path: \"" + fileUrl.toString() + "\"");
     qDebug() << fileUrl;
     QNetworkRequest request(fileUrl);
-    nam->get(request);
+
+    QNetworkReply *repl =  nam->get(request);
+    connect(repl, &QNetworkReply::downloadProgress,
+            [](qint64 bytesReceived, qint64 bytesTotal){
+        qDebug("download process %d %d", bytesReceived, bytesTotal);
+    });
+
 
 }
 
@@ -1561,3 +1583,89 @@ void Dialog::unZip(QString zip_filename , QString filename)
    infile.close();
    outfile.close();
 }
+
+//bool extract(const QString & filePath, const QString & extDirPath, const QString & singleFileName)
+//{
+
+//    QuaZip zip(filePath);
+
+//    if (!zip.open(QuaZip::mdUnzip)) {
+//        qWarning("testRead(): zip.open(): %d", zip.getZipError());
+//        return false;
+//    }
+
+//    zip.setFileNameCodec("IBM866");
+
+//    qWarning("%d entries\n", zip.getEntriesCount());
+//    qWarning("Global comment: %s\n", zip.getComment().toLocal8Bit().constData());
+
+//    QuaZipFileInfo info;
+
+//    QuaZipFile file(&zip);
+
+//    QFile out;
+//    QString name;
+//    char c;
+//    for (bool more = zip.goToFirstFile(); more; more = zip.goToNextFile()) {
+
+//        if (!zip.getCurrentFileInfo(&info)) {
+//            qWarning("testRead(): getCurrentFileInfo(): %d\n", zip.getZipError());
+//            return false;
+//        }
+
+//        if (!singleFileName.isEmpty())
+//            if (!info.name.contains(singleFileName))
+//                continue;
+
+//        if (!file.open(QIODevice::ReadOnly)) {
+//            qWarning("testRead(): file.open(): %d", file.getZipError());
+//            return false;
+//        }
+
+//        name = QString("%1/%2").arg(extDirPath).arg(file.getActualFileName());
+
+//        if (file.getZipError() != UNZ_OK) {
+//            qWarning("testRead(): file.getFileName(): %d", file.getZipError());
+//            return false;
+//        }
+
+//        //out.setFileName("out/" + name);
+//        out.setFileName(name);
+
+//        // this will fail if "name" contains subdirectories, but we don't mind that
+//        out.open(QIODevice::WriteOnly);
+//        // Slow like hell (on GNU/Linux at least), but it is not my fault.
+//        // Not ZIP/UNZIP package's fault either.
+//        // The slowest thing here is out.putChar(c).
+//        while (file.getChar(&c)) out.putChar(c);
+
+//        out.close();
+
+//        if (file.getZipError() != UNZ_OK) {
+//            qWarning("testRead(): file.getFileName(): %d", file.getZipError());
+//            return false;
+//        }
+
+//        if (!file.atEnd()) {
+//            qWarning("testRead(): read all but not EOF");
+//            return false;
+//        }
+
+//        file.close();
+
+//        if (file.getZipError() != UNZ_OK) {
+//            qWarning("testRead(): file.close(): %d", file.getZipError());
+//            return false;
+//        }
+//    }
+
+//    zip.close();
+
+//    if (zip.getZipError() != UNZ_OK) {
+//        qWarning("testRead(): zip.close(): %d", zip.getZipError());
+//        return false;
+//    }
+
+//    return true;
+//}
+
